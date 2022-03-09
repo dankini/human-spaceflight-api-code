20473442461bdcab260a574e560ea028c6021dac
Dan Martinez <danjmartinez@hotmail.com>
2022-03-09 17:40:56 +0000

    feat(): Apply styling to pagination in list templates

--- static/apis_general/js/_base_apis_general.js
+++ static/apis_general/js/_base_apis_general.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     //Pagination numbers
     $('#paginationSimpleNumbers').DataTable({
         "pagingType": "simple_numbers",
+        /*"searching": false,*/
+        "lengthChange": false,
         "ordering": false // false to disable sorting (or any other option)
     });
 });
